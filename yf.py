6dc5c2e51f7f87c95c9db4f70bc71560af5c43c1
penkod <dejan.penko@lecad.fs.uni-lj.si>
2020-10-22 20:06:28 +0200

    Add 'close vs open' and 'current vs close' and add euro and percentage symbols.

--- yf.py
+++ yf.py
@@ -10,43 +10,36 @@ def ExtractStocksData():
     import yfinance as yf
 
     stockSymbols = ["AMD",
-                   "INTC",
-                   "NVDA",
-                   "MX",
-                   "NXPI",
-                   "SOXX",
-                   "TSLA",
-                   "GOOG",
-                   "SPOT",
-                   "NOK",
-                   "PYPL",
-                   "ADSK",
-                   "ADBE",
-                   "ANSS",
-                   "CSCO",
-                   "DELL",
-                   "QCOM",
-                   "MSFT",
-                   "WDC",
-                   "TXN"]
+                    "INTC",
+                    "NVDA",
+                    "MX",
+                    "NXPI",
+                    "SOXX",
+                    "TSLA",
+                    "GOOG",
+                    "SPOT",
+                    "NOK",
+                    "PYPL",
+                    "ADSK",
+                    "ADBE",
+                    "ANSS",
+                    "CSCO",
+                    "DELL",
+                    "QCOM",
+                    "MSFT",
+                    "WDC",
+                    "TXN"]
     # dummies
-    data_1d = [0]*len(stockSymbols)
-    data_7d = [0]*len(stockSymbols)
-    data_30d = [0]*len(stockSymbols)
     data_12mo = [0]*len(stockSymbols)
     current = [0]*len(stockSymbols)
 
     i = 0
     for stock in stockSymbols:
 
-        data_1d[i] = yf.download(stock, period="1d")
-        data_7d[i] = yf.download(stock, period="7d")
-        data_30d[i] = yf.download(stock, period="30d")
         data_12mo[i] = yf.download(stock, period="12mo")
 
         # keys: ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
         print(max(data_12mo[i]["High"]))
-        print(data_1d[i])
 
         # Get current price
         ticker_yahoo = yf.Ticker(stock)
@@ -58,40 +51,53 @@ def ExtractStocksData():
 
         i += 1
 
-    return stockSymbols, current, data_1d, data_7d, data_30d, data_12mo
+    return stockSymbols, current, data_12mo
 
 
 def filterStocks():
+
+    import numpy as np
+
     # Filter stocks
-    stockSymbols, current, data_1d, data_7d, data_30d, data_12mo = ExtractStocksData()
+    stockSymbols, current, data_12mo = ExtractStocksData()
 
     filteredSymbols = []
     filteredPrevCloses = []
     filteredOpens = []
     filteredCurrents = []
+    filteredOpenVsPrevClose = []
+    filteredOpenVsCurrent = []
     for i in range(len(stockSymbols)):
-        print("Open: ", data_1d[i]["Open"][0])
-        print("Close: ", data_1d[i]["Close"][0])
-        premarket_up_percent = (data_1d[i]["Open"][0]/data_1d[i]["Close"][0]-1)*100
-        current_percent = (current[i]/data_1d[i]["Close"][0]-1)*100
+        print("Open: ", data_12mo[i]["Open"][-1])
+        print("Close: ", data_12mo[i]["Close"][-2])
+        premarket_up_percent = \
+            (data_12mo[i]["Open"][-1]/data_12mo[i]["Close"][-2]-1)*100
+        current_percent = (current[i]/data_12mo[i]["Close"][-2]-1)*100
 
         print("premarket_up_percent: ", premarket_up_percent)
         print("current_percent: ", current_percent)
 
-        if (current_percent > premarket_up_percent) and ((current_percent - premarket_up_percent) > 0.5):
+        if (current_percent > premarket_up_percent) and \
+           ((current_percent - premarket_up_percent) > 0.5):
             filteredSymbols.append(stockSymbols[i])
-            filteredPrevCloses.append(data_1d[i]["Close"][0])
-            filteredOpens.append(data_1d[i]["Open"][0])
-            filteredCurrents.append(current[i])
+            filteredPrevCloses.append(np.round(data_12mo[i]["Close"][-2], 2))
+            filteredOpens.append(np.round(data_12mo[i]["Open"][-1], 2))
+            filteredCurrents.append(np.round(current[i], 2))
+            filteredOpenVsPrevClose.append(
+                np.round((filteredOpens[-1]/filteredPrevCloses[-1]-1)*100, 2))
+            filteredOpenVsCurrent.append(
+                np.round((filteredCurrents[-1]/filteredPrevCloses[-1]-1)*100, 2))
 
         zipped = zip(filteredSymbols, filteredPrevCloses, filteredOpens,
-                     filteredCurrents)
+                     filteredCurrents, filteredOpenVsPrevClose,
+                     filteredOpenVsCurrent)
 
     return zipped
 
 
 if __name__ == "__main__":
-    # stockSymbols, current, data_1d, data_7d, data_30d, data_12mo = ExtractStocksData()
+    # test
+    # stockSymbols, current, data_12mo = ExtractStocksData()
     # print(stockSymbols)
     zipped = filterStocks()
     print(zipped)
